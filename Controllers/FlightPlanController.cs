83b0618187d74ac99e59e8a29dd9597d2a4b580c
winniethong <winniethong06@gmail.com>
2024-11-18 20:16:42 -0800

    task: define api methods

--- Controllers/FlightPlanController.cs
+++ Controllers/FlightPlanController.cs
@@ -12,5 +12,37 @@ namespace FlightPlanApi.Controllers
         public FlightPlanController(IDatabaseAdapter database){
             _database = database;
         }
+
+        public async Task<IActionResult> FlightPlanList(){
+
+        }
+
+        public async Task<IActionResult> GetFlightPlanById(string flightPlanId){
+
+        }
+
+        public async Task<IActionResult> FileFlightPlan(FlightPlan flightplan){
+
+        }
+
+        public async Task<IActionResult> UpdateFileFlightPlan(FlightPlanApi flightplan){
+
+        }
+
+        public async Tasl<IActionResult> DeleteFlightPlan( string flightPlanId){
+
+        }
+
+        public async Tasl<IActionResult> GetFlightPlanDepartureAirport( string flightPlanId){
+            
+        }
+
+        public async Tasl<IActionResult> GetFlightPlanRoute( string flightPlanId){
+            
+        }
+
+        public async Tasl<IActionResult> GetFlightPlanTimeEnroute( string flightPlanId){
+            
+        }
     }
 }
